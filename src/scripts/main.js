78011c375affc1fc3279bd00705731cf2c8ab560
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2025-01-22 22:52:29 +0200

    Create header with navigation, hero section with slider; add reset styles, typography styles, container styles, and utilities (mixins, placeholders, variables)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+
+// slider
+const slidesList = document.querySelector('.slider__list');
+const slides = document.querySelectorAll('.slider__item');
+const prevBtn = document.querySelector('.slider__button--prev');
+const nextBtn = document.querySelector('.slider__button--next');
+
+let currentSlide = 0;
+
+function updateSliderPosition() {
+  slidesList.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+prevBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1;
+  updateSliderPosition();
+});
+
+nextBtn.addEventListener('click', () => {
+  currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0;
+  updateSliderPosition();
+});
